75b846ab50ab0e2cecaabe2838a5920d0da9a06d
moaizmm <moaiz_malik@hotmail.com 1enjkdlgit config --global user.email moaiz_malik@hotmail.com>
2024-01-05 00:33:12 +0000

    Updates aks-cluster-module outputs.tf.

--- aks-terraform/aks-cluster-module/outputs.tf
+++ aks-terraform/aks-cluster-module/outputs.tf
@@ -2,15 +2,15 @@
 
 output "aks_cluster_name" {
     description = "Name of the AKS cluster"
-    value = azure_aks_cluster.aks_cluster.name
+    value = azurerm_kubernetes_cluster.aks_cluster.name
 }
 
 output "aks_cluster_id" {
     description = "ID of the AKS cluster"
-    value = azure_aks_cluster.aks_cluster.id
+    value = azurerm_kubernetes_cluster.aks_cluster.id
 }
 
 output "aks_kubeconfig" {
     description = "Kubeconfig file for accessing the AKS cluster"
-    value = azure_aks_cluster.aks_cluster.kube_config_raw
+    value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
 }
