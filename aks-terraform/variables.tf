aa58718167c83d910870096a231e8d00e8416155
moaizmm <148896480+moaizmm@users.noreply.github.com>
2024-01-17 22:31:01 +0000

    Update variables.tf

--- aks-terraform/variables.tf
+++ aks-terraform/variables.tf
@@ -11,3 +11,14 @@ variable "client_secret" {
   type        = string
   sensitive   = true
 }
+variable "service_principal_client_id" {
+  description = "Service principal client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "service_principal_client_secret" {
+  description = "Secret key for the service principal"
+  type        = string
+  sensitive   = true
+}
